90f1c49c5d8477d73311e9c0f28a8f73b1c98ba2
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-01-10 19:57:29 +0545

    added serve-static

--- app/index.js
+++ app/index.js
@@ -5,14 +5,16 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || process.env.port || 8080;
 
-app.get("/", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
 app.listen(port, () => {
   console.log(
     `${chalk.greenBright("[Manager]")} ${chalk.green(
-      "listening on http://localhost:${port}"
+      `listening on http://localhost:${port}`
     )}`
   );
 });
