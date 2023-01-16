0e9b67ee5cc972d676ec4c3e0d732c21ad538e15
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-01-16 14:52:14 +0545

    added script for index file

---
+++ app/public/js/script.js
@@ -0,0 +1,11 @@
+const btc = document.getElementById("btc");
+const wifi = document.getElementById("wifi");
+const text = document.getElementById("text");
+const vcard = document.getElementById("vcard");
+const email = document.getElementById("email");
+
+btc.onclick = () => location.href = "/btc";
+wifi.onclick = () => location.href = "/wifi";
+text.onclick = () => location.href = "/text";
+vcard.onclick = () => location.href = "/vcard";
+email.onclick = () => location.href = "/email";
